f5797930fde5127febc430cad0b5f1ee57f69c9a
a24lucasba <a24lucasba@iessanclemente.net>
2025-11-28 14:32:35 +0100

    28/11/25

---
+++ ned/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+ip route del default via 192.168.10.1
+ip route add default via 192.168.10.254
+
+
+rm -f /var/lib/kea/dhcp4.kea-dhcp-leases.csv
+
+
+kea-dhcp4-ddns -c /etc/kea/ddns.conf -d &
+kea-dhcp4 -c /etc/kea/dhcp4.conf -d
