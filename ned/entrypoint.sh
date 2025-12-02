f20605da38d07772bfea657df7fcd89e2ec8f408
a24lucasba <a24lucasba@iessanclemente.net>
2025-12-02 10:27:10 +0100

    DHCP y DDNS funcinal (ahora sí) y logs

--- ned/entrypoint.sh
+++ ned/entrypoint.sh
@@ -3,11 +3,13 @@
 ip route del default via 192.168.10.1
 ip route add default via 192.168.10.254
 
+rm -f /var/log/kea/kea-dhcp-debug.log
+rm -f /var/log/kea/kea-ddns-debug.log
 
 rm -f /var/lib/kea/dhcp4.kea-dhcp-leases.csv
 
 rm -f /var/run/kea/dhcp4.kea-dhcp4.pid
 rm -f /var/run/kea-dhcp-ddns.kea-dhcp-ddns.pid
 
-kea-dhcp4-ddns -c /etc/kea/ddns.conf -d &
+kea-dhcp-ddns -c /etc/kea/ddns.conf -d &
 kea-dhcp4 -c /etc/kea/dhcp4.conf -d
