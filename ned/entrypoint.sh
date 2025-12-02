4b7b38625e76792d6df50f15b64f92297804bc0a
a24lucasba <a24lucasba@iessanclemente.net>
2025-12-02 09:22:38 +0100

    DHCP y DDNS funcional

--- ned/entrypoint.sh
+++ ned/entrypoint.sh
@@ -6,6 +6,8 @@ ip route add default via 192.168.10.254
 
 rm -f /var/lib/kea/dhcp4.kea-dhcp-leases.csv
 
+rm -f /var/run/kea/dhcp4.kea-dhcp4.pid
+rm -f /var/run/kea-dhcp-ddns.kea-dhcp-ddns.pid
 
 kea-dhcp4-ddns -c /etc/kea/ddns.conf -d &
 kea-dhcp4 -c /etc/kea/dhcp4.conf -d
